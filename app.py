bbedb0448aec0cca44743ced866119a432208779
UGADHI A <77853496+ugadhi@users.noreply.github.com>
2021-08-07 13:43:32 +0530

    updated id

---
+++ app.py
@@ -0,0 +1,37 @@
+import asyncio  
+import sys 
+from flask import Flask, request, Response  
+from botbuilder.core import (  
+    BotFrameworkAdapter,  
+    BotFrameworkAdapterSettings,     
+    TurnContext,      
+)  
+rom botbuilder.schema import Activity  
+from echobot import*  
+bot = EchoBot()  
+SETTINGS = BotFrameworkAdapterSettings("","")  
+ADAPTER = BotFrameworkAdapter(SETTINGS)  
+LOOP = asyncio.get_event_loop()  
+@app.route("/api/messages", methods=["POST"])  
+def messages():  
+    if "application/json" in request.headers["Content-Type"]:  
+        body = request.json  
+    else:  
+        return Response(status=415)  
+  
+    activity = Activity().deserialize(body)  
+    auth_header = (  
+        request.headers["Authorization"] if "Authorization" in request.headers else ""  
+    )  
+  
+    async def aux_func(turn_context):  
+        await bot.on_turn(turn_context)  
+  
+    try:  
+        task = LOOP.create_task(  
+            ADAPTER.process_activity(activity, auth_header, aux_func)  
+        )  
+        LOOP.run_until_complete(task)  
+        return Response(status=201)  
+    except Exception as exception:  
+        raise exception  
